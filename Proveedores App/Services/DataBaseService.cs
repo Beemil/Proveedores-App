89b19f5372825166e33dccdd296cd8e5b47cd694
Beemil <valeskavr17@gmail.com>
2025-09-26 20:19:00 -0600

    Base de datos

---
+++ Proveedores App/Services/DataBaseService.cs
@@ -0,0 +1,37 @@
+﻿using Proveedores_App.Models;
+using SQLite;
+
+namespace Proveedores_App.Services
+{
+    public class DataBaseService : IDataBaseService
+    {
+        private SQLiteAsyncConnection _db;
+
+        public DataBaseService()
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Proveedores.db3");
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Proveedores>();
+        }
+
+        public async Task<int> CreateProveedor(Proveedores proveedor)
+        {
+            return await _db.InsertAsync(proveedor);
+        }
+
+        public async Task<int> DeleteProveedor(Proveedores proveedor)
+        {
+            return await _db.DeleteAsync(proveedor);
+        }
+
+        public async Task<List<Proveedores>> GetAllProveedores()
+        {
+            return await _db.Table<Proveedores>().ToListAsync();
+        }
+
+        public async Task<int> UpdateProveedor(Proveedores proveedor)
+        {
+            return await _db.UpdateAsync(proveedor);
+        }
+    }
+}
---
+++ Proveedores App/Services/IDataBaseService.cs
@@ -0,0 +1,37 @@
+﻿using Proveedores_App.Models;
+
+namespace Proveedores_App.Services
+{
+    /// <summary>
+    /// Interfaz donde están los métodos para el uso con la base de datos
+    /// </summary>
+    public interface IDataBaseService
+    {
+        /// <summary>
+        /// Obtener todo el listado de proveedores
+        /// </summary>
+        /// <returns>Listado de proveedores</returns>
+        public Task<List<Proveedores>> GetAllProveedores();
+
+        /// <summary>
+        /// Crea un nuevo proveedor
+        /// </summary>
+        /// <param name="proveedor">Objeto con los registros a crear</param>
+        /// <returns>Número de proveedores creados</returns>
+        public Task<int> CreateProveedor(Proveedores proveedor);
+
+        /// <summary>
+        /// Actualiza un proveedor
+        /// </summary>
+        /// <param name="proveedor">Objeto con los registros a actualizar</param>
+        /// <returns>Número de proveedores actualizados</returns>
+        public Task<int> UpdateProveedor(Proveedores proveedor);
+
+        /// <summary>
+        /// Elimina un proveedor
+        /// </summary>
+        /// <param name="proveedor">Objeto del proveedor a eliminar</param>
+        /// <returns>Número de proveedores eliminados</returns>
+        public Task<int> DeleteProveedor(Proveedores proveedor);
+    }
+}
