816540c21bb1fd090021b39a2df536bb923a7686
Beemil <valeskavr17@gmail.com>
2025-09-26 20:23:07 -0600

    Create Proveedores.cs

---
+++ Proveedores App/Models/Proveedores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Proveedores_App.Models
+{
+    public class Proveedores
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Producto { get; set; }
+    }
+}
