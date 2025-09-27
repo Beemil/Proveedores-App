3123145b1878083176dbab63d4c5d3f528c004e0
Beemil <valeskavr17@gmail.com>
2025-09-26 20:32:13 -0600

    pricipals

---
+++ Proveedores App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Proveedores_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new Proveedores_App.Views.ProveedoresView();
+        }
+
+    }
+}
---
+++ Proveedores App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Proveedores_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
